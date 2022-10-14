1194129be51fe722be7052b51eb66ff782c97124
Olga Zhuravleva <onpol@mail.ru>
2022-10-14 14:35:39 +0300

    dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
+  if (Array.isArray(members))
+  {
+    let name = members.map(function(str) { 
+        if (typeof(str) == 'string') return str.trim().toUpperCase()[0]; 
+      }).sort().join ('');
+    return name;
+  }
+  else
+    return false;
 }
 
 module.exports = {
