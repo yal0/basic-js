7cc3cd8c6e4201c1825074a96ab7ac852e31597c
Olga Zhuravleva <onpol@mail.ru>
2022-10-14 15:38:16 +0300

    carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(typeof(sampleActivity) == 'string' && Number(sampleActivity) && sampleActivity<15 && sampleActivity>0 )
+    return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD));
+  else
+    return false;
 }
 
 module.exports = {
