8e73a8db7116cd0e6aeee76276ebd47f8fbf25fa
Olga Zhuravleva <onpol@mail.ru>
2022-10-14 22:21:31 +0300

    simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  chain : [],
+
+   getLength() {
+     return this.chain.length;
+   },
+   addLink(value = ' ') {
+      this.chain.push(`( ${value} )`);
+      return this;
+   },
+  removeLink(position) {
+    if (!Number.isInteger(position) || (position <= 0) || position > this.getLength()) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chainStr = this.chain.join('~~');
+    this.chain = [];
+    return chainStr;
   }
+
 };
 
 module.exports = {
